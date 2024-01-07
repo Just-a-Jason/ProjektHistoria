a9115196ef99a21dce41c0b9269fa593ad75ccba
Kuguar2 <kuguar.2137@gmail.com>
2024-01-07 16:05:27 +0100

    navbar change

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,14 +1,15 @@
 import NavBarButton from "./NavBarButton";
 import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <section className="Links">
-        <div className="navbar__location">
-          <h2>
+      <ul className="navbarList">
+        <li className="navbarButton">
+          <Link to="/">
             Strona główna
-            <svg width="20" height="20" viewBox="0 0 36 36">
+            <svg width="20" height="15" viewBox="0 0 36 36">
               <path
                 fill="currentColor"
                 d="m33.71 17.29l-15-15a1 1 0 0 0-1.41 0l-15 15a1 1 0 0 0 1.41 1.41L18 4.41l14.29 14.3a1 1 0 0 0 1.41-1.41Z"
@@ -19,15 +20,29 @@ const NavBar = () => {
               />
               <path fill="none" d="M0 0h36v36H0z" />
             </svg>
-          </h2>
+          </Link>
+        </li>
+        <div className="mainPages">
+          <li className="navbarButton">
+            <NavBarButton link="/Temat1" text="Temat 1" />
+          </li>
+          <li className="navbarButton">
+            <NavBarButton link="/Temat2" text="Temat 2" />
+          </li>
+          <li className="navbarButton">
+            <NavBarButton link="/Temat3" text="Temat 3" />
+          </li>
+          <li className="navbarButton">
+            <NavBarButton link="/Temat4" text="Temat 4" />
+          </li>
+          <li className="navbarButton">
+            <NavBarButton link="/Temat5" text="Temat 5" />
+          </li>
+          <li className="navbarButton">
+            <NavBarButton link="/Temat6" text="Temat 6" />
+          </li>
         </div>
-        <NavBarButton link="/Temat1" text="Temat 1" />
-        <NavBarButton link="/Temat2" text="Temat 2" />
-        <NavBarButton link="/Temat3" text="Temat 3" />
-        <NavBarButton link="/Temat4" text="Temat 4" />
-        <NavBarButton link="/Temat5" text="Temat 5" />
-        <NavBarButton link="/Temat6" text="Temat 6" />
-      </section>
+      </ul>
     </nav>
   );
 };
