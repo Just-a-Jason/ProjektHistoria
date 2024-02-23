d2659fa5ac6783f3895f8ae8fd77af52e03f69a2
Kuguar2 <kuguar.2137@gmail.com>
2024-02-23 17:56:41 +0100

    temat 5 uzupełnienie tekstu

--- src/Pages/Temat5.tsx
+++ src/Pages/Temat5.tsx
@@ -1,12 +1,83 @@
+import "../Components/NavBar.css";
 const Temat5 = () => {
   return (
-    <>
-      <h1>Wiosna Ludów na ziemiach polskich</h1>
+    <div className="Temat5Body">
+      <h2>Wiosna Ludów na ziemiach polskich</h2>
       <p>
         Emisariusze Towarzystwa Demokratycznego Polskiego z zagranicy prowadzili
-        spiskową działalność
+        spiskową działalność z myślą o zbrojnym powstaniu. Jego wybuch planowano
+        we wszystkich zaborach równocześnie a sam termin powstania został
+        ustanowiony na 1846r. lecz ze względu na donos do władz prus i rozbicie
+        przez Rosjan spisku na Litwie doszło jedynie do pojedyńczych wystąpień
+        na Galicji i w Królestwie Kongresowym a dzięki umiejętnemu podsycaniu
+        konfliktu pomiędzy chłopami a polskimi ziemianami przez władze
+        austriackie powstańcy Galicyjscy musieli walczyć nie tylko z żołnierzami
+        austriackimi lecz również z chłopami, którzy mordowali szlachtę, palili
+        dwory i niszczyli wykazy powinności przez co przeprowadzenie powstania
+        stało się niemożliwe.
       </p>
-    </>
+      <p>
+        Mimo trwającej w Galicji rabacji w pozostałych zaborach 21 lutego 1846r.
+        wybuchło powstanie w Krakowie. Powstał wówczas Rząd Narodowy
+        Rzeczypospolitej Polskiej a na jego czele stanął Jan Tyssowski.Wkrótce
+        Rząd Narodowy wydał Manifest do narodu polskiego. Zapowiadano w nim
+        równość wobec prawa, nadanie chłopom użytkowanej ziemi na własnoś,
+        zniesienie pańszczyzny i pomoc społeczną dla mieszkańców miast. Pod
+        koniec lutego sytuacja powstańców stała się tragiczna więc 27 lutego pod
+        wodzą Edwarda Dębowskiego wyruszyła pokojowa procesja, która miała
+        zjednać chłopów dla idei walki narodowej lecz mimo to modlący się tłum
+        został ostrzelany przez Austriaków i jest to symboliczny koniec
+        powstania w wyniku czego Wolne Miasto Kraków zostało włączone do
+        Austrii.
+      </p>
+      <img src="src\Images\WLmap.jpg" className="Temat5Img"></img>
+      <p>
+        Wydarzenia Wiosny Ludów w Wiedniu, a zwłaszcza rewolucja w Berlinie
+        spowodowały wzrost patriotycznych nastrojów w Wielkopolsce. W marcu
+        1848r. na ulice Poznania wyległy tłumy demonstrantów a władze pruskie,
+        skupione na wystąpieniach w Berlinie i innych niemieckich miastach
+        ,początkowo nie interweniowały.W poznańskim bazarze powstał Komitet
+        Narodowy oraz utworzono polskie oddziały zbrojne.Do Berlina udała się
+        polska delegacja gdzie pruski król zgodził się na przywrócenie polskiej
+        administracji, utworzenie gwardii narodowej i wycofanie wojska do koszar
+        lecz były to tylko pozory dobrej woli aby opóźnić wystąpienie polskich
+        poddanych. 11 kwietnia strona polska w zamian za zgodę na autonomię
+        zobowiązała się do reorganizacji swojego wojska i pozostawienia czterech
+        obozów lecz strona purska nie miała zamiaru przestrzegać porozumienia i
+        zaczeła likwidować polskie wystąpienia oraz wojsko pruskie mimo
+        oficjalnych ustaleń dopuszczało się aktów przemocy na polakach. 21
+        kwietnia Prusacy zażądali rozwiązania wszystkich polskich komitetów,
+        spotkało się to ze sprzeciwem i 29 kwietnia Prusacy zaatakowali polski
+        obóz w Książu. Następnego dnia Polacy pod dowództwem Ludwika
+        Mierosławskiego pokonali część wojsk pruskich, 2 maja zdobyli Wrześnie,
+        a następnie ponownie pokonali Prusaków pod Sokołowem. Mimo tych
+        zwycięstw powstanie upadło gdyż oficerowi nie byli chętni dalszym walkom
+        a ziemiaństwo bało się późniejszych represji.
+      </p>
+      <p>
+        Informacje docierające z ogarniętego rewolucją Wiednia zachęcały do
+        wystąpienia przeciwko osłabionemu zaborcy.Na przełomie marca i kwietnia
+        1848 r. we Lwowie i Krakowie powstały polskie organizacje,które miały
+        reprezentować Polaków w relacjach z austriackimi władzami.Zaczęto
+        formować polską Gwardię Narodową a do Wiednia udała się polska
+        delegacja,która uzyskała od cesarza obietnicę polonizacji administracji
+        i szkół.Nauczeni wcześniejszym doświadczeniem polscy działacze
+        zaapelowali więc do ziemian o zwalnianie chłopów z pańszczyźnianych
+        obciążeń.Gubernator Galicji Franz von Stadion z niepokojem przyglądał
+        się sytuacji,która mogła doprowadzić do zakończenia rozniecanego od lat
+        przez władze zaborcze konfliktu pomiędzy dworem a wsią.Ze względu na
+        brak reakcji Wiednia postanowił on wziąć sprawy w swoje ręce i 22
+        kwietnia 1848 r. ogłosił zniesienie pańszczyzny i zapowiedział
+        uwłaszczenie chłopów.Pod koniec kwietnia doszło do krótkotrwałych walk i
+        Austriacy 26 kwietnia ostrzelali Kraków z artylerii, zmuszając Polaków
+        do kapitulacji.Po stłumieniu rewolucji w Wiedniu jesienią 1848 r.
+        Austriacy przystąpili do pacyfikacji Lwowa.Polskie instytucje przestały
+        istnieć a a represje objęły całą Galicję.Pozytywnymi kwestiami Wiosny
+        Ludów w zaborze austriackim było znalezienie się Polskiej reprezentacji
+        w sejmie wiedeńskim,a Gubernatorem Galicji został pierwszy Polak na tym
+        stanowisku Wacław Zaleski.
+      </p>
+    </div>
   );
 };
 
